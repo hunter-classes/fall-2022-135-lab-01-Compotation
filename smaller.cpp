1acf8e5ce31b06d011a9e62abe433a5107e52d64
Compotation <46853874+Compotation@users.noreply.github.com>
2022-08-29 12:48:17 -0400

    print out smallest

--- smaller.cpp
+++ smaller.cpp
@@ -8,6 +8,12 @@ int main() {
 	
 	std::cout << "Second number: ";
 	std::cin >> second_num;
+	
+	if (first_num < second_num) {
+		std::cout << first_num << std::endl;
+		return 0;
+	}
+	std::cout << second_num << std::endl;
 
 	return 0;
 }
