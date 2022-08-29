42a956ca7f8b83e46730ff7f63b190feed7514af
Compotation <46853874+Compotation@users.noreply.github.com>
2022-08-29 12:31:46 -0400

    ask for 2 numbers

---
+++ smaller.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int first_num;
+	int second_num;
+	std::cout << "First number: ";
+	std::cin >> first_num;
+	
+	std::cout << "Second number: ";
+	std::cin >> second_num;
+
+	return 0;
+}
