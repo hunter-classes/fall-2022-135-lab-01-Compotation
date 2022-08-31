d908c6429d6dba907fc56e382b899c4c1839514c
Compotation <46853874+Compotation@users.noreply.github.com>
2022-08-31 15:03:08 -0400

    all months with 30 days works

--- month.cpp
+++ month.cpp
@@ -10,6 +10,8 @@ int main() {
 
 	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
 		std::cout << "31" << std::endl;
+	} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+		std::cout << "30" << std::endl;
 	}
 	return 0;
 }
