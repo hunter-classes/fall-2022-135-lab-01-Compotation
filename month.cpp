3c3fb62e00d8e1fbe5fce1217c035cc515eee226
Compotation <46853874+Compotation@users.noreply.github.com>
2022-08-31 15:20:23 -0400

    add "days" to output

--- month.cpp
+++ month.cpp
@@ -9,20 +9,20 @@ int main() {
 	std::cin >> month;
 
 	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-		std::cout << "31" << std::endl;
+		std::cout << "31 days" << std::endl;
 	} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-		std::cout << "30" << std::endl;
+		std::cout << "30 days" << std::endl;
 	} else {
 		if (year % 100 == 0) {
                 	if (year % 400 == 0) {
-				std::cout << "29" << std::endl;
+				std::cout << "29 days" << std::endl;
                 	} else {
-				std::cout << "28" << std::endl;
+				std::cout << "28 days" << std::endl;
 			}
         	} else if (year % 4 == 0) {
-			std::cout << "29" << std::endl;
+			std::cout << "29 days" << std::endl;
         	} else {
-			std::cout << "28" << std::endl;
+			std::cout << "28 days" << std::endl;
 		}
 	}
 	return 0;
