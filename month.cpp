40e4b58b4ea8dccb8e0bd57b4b16f89eac9d60f4
Compotation <46853874+Compotation@users.noreply.github.com>
2022-08-31 15:16:35 -0400

    calculate days for february

--- month.cpp
+++ month.cpp
@@ -12,6 +12,18 @@ int main() {
 		std::cout << "31" << std::endl;
 	} else if (month == 4 || month == 6 || month == 9 || month == 11) {
 		std::cout << "30" << std::endl;
+	} else {
+		if (year % 100 == 0) {
+                	if (year % 400 == 0) {
+				std::cout << "29" << std::endl;
+                	} else {
+				std::cout << "28" << std::endl;
+			}
+        	} else if (year % 4 == 0) {
+			std::cout << "29" << std::endl;
+        	} else {
+			std::cout << "28" << std::endl;
+		}
 	}
 	return 0;
 }
